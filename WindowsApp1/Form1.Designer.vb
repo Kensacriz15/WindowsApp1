687bb1372024f969cd05142e4e161c37bd39cef7
MIS - Rafael <MIS - Rafael@MIS-PETER>
2024-05-09 16:52:56 +0800

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.Home = New System.Windows.Forms.TabPage()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Panel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.Home.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.LightBlue
+        Me.Panel2.Location = New System.Drawing.Point(2, -2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(797, 103)
+        Me.Panel2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(135, 26)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Home"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(0, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(135, 26)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Ticket"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 97)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(135, 356)
+        Me.Panel1.TabIndex = 1
+        '
+        'TabControl1
+        '
+        Me.TabControl1.AccessibleName = ""
+        Me.TabControl1.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.TabControl1.Controls.Add(Me.Home)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(133, 72)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(670, 381)
+        Me.TabControl1.TabIndex = 3
+        Me.TabControl1.Tag = ""
+        '
+        'Home
+        '
+        Me.Home.Controls.Add(Me.Button5)
+        Me.Home.Controls.Add(Me.ComboBox2)
+        Me.Home.Controls.Add(Me.RichTextBox1)
+        Me.Home.Controls.Add(Me.TextBox2)
+        Me.Home.Controls.Add(Me.Button4)
+        Me.Home.Controls.Add(Me.TextBox1)
+        Me.Home.Controls.Add(Me.ComboBox1)
+        Me.Home.Location = New System.Drawing.Point(4, 22)
+        Me.Home.Name = "Home"
+        Me.Home.Padding = New System.Windows.Forms.Padding(3)
+        Me.Home.Size = New System.Drawing.Size(662, 355)
+        Me.Home.TabIndex = 0
+        Me.Home.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button5.Location = New System.Drawing.Point(305, 231)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Save Report"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"LOW", "URGENT", "CRITICAL"})
+        Me.ComboBox2.Location = New System.Drawing.Point(269, 195)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 6
+        Me.ComboBox2.Text = "Level"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(151, 102)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(348, 87)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = "Description(Problem)"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(151, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(142, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Name"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(178, 24)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(36, 23)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "+"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(240, 26)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(163, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"CSMC", "MKK", "SPIRAL"})
+        Me.ComboBox1.Location = New System.Drawing.Point(378, 63)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "Department"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DateTimePicker1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(662, 355)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(230, 74)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "MIS MAINTENANCE"
+        Me.Panel1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.Home.ResumeLayout(False)
+        Me.Home.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents Home As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TabControl1.SelectedIndex = 1
+    End Sub
+
+    Private Sub TabPage2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Dim form1 As New Form1()
+        form1.Show()
+    End Sub
+
+    Private UsedNumbers As New List(Of Long)()
+
+    Public Function GenerateUnique5DigitNumber() As Long
+        Dim random As New Random()
+        Dim newNumber As Long
+
+        Do
+            newNumber = random.Next(10001, 100000)
+        Loop While UsedNumbers.Contains(newNumber)
+
+        UsedNumbers.Add(newNumber)
+        Return newNumber
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim randomNumber As Long = GenerateUnique5DigitNumber()
+        TextBox1.Text = randomNumber.ToString()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        'Generate Ticket Number
+        TextBox1.ReadOnly = True
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        'Name
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        'Department
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        'Level
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        'Description
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim filePath As String = "C:\Users\MIS - Rafael\Desktop\PLDT\test.txt"
+        Dim data As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") & "|" & TextBox1.Text & "|" & TextBox2.Text & "|" & ComboBox1.Text & "|" & ComboBox2.Text & "|" & RichTextBox1.Text
+
+        If File.Exists(filePath) Then
+            File.AppendAllText(filePath, Environment.NewLine & data)
+        Else
+            File.WriteAllText(filePath, data)
+        End If
+
+        Dim values() As String = File.ReadAllLines(filePath)
+        Dim lastEntry As String = values(values.Length - 1)
+        Dim lastEntryValues() As String = lastEntry.Split("|"c)
+        TextBox1.Text = lastEntryValues(1)
+        TextBox2.Text = lastEntryValues(2)
+        ComboBox1.Text = lastEntryValues(3)
+        ComboBox2.Text = lastEntryValues(4)
+        RichTextBox1.Text = lastEntryValues(5)
+        MessageBox.Show("Report Ticket Submitted")
+
+    End Sub
+End Class
